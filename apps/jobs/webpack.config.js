accf58467c8003cff7489838d730329ec981f030
camou92 <cmohamed992@gmail.com>
2026-03-07 17:16:40 +0000

    feat: update dockerfile jobs

--- apps/jobs/webpack.config.js
+++ apps/jobs/webpack.config.js
@@ -1,4 +1,4 @@
-const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
+/* const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
 const { join } = require('path');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const { resolve } = require('path');
@@ -26,5 +26,41 @@ module.exports = {
       generatePackageJson: false,
       sourceMap: true,
     })
+  ],
+};
+ */
+
+const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
+const { resolve, join } = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+module.exports = {
+  resolve: {
+    plugins: [
+      new TsconfigPathsPlugin({ configFile: resolve(__dirname, 'tsconfig.app.json') }),
+    ],
+  },
+  output: {
+    path: join(__dirname, 'dist'), // le dist de l'app
+    clean: true,
+    filename: 'main.js', // fichier final unique
+    ...(process.env.NODE_ENV !== 'production' && {
+      devtoolModuleFilenameTemplate: '[absolute-resource-path]',
+    }),
+  },
+  plugins: [
+    new NxAppWebpackPlugin({
+      target: 'node',
+      compiler: 'tsc',
+      main: './src/main.ts',
+      tsConfig: resolve(__dirname, 'tsconfig.app.json'),
+      assets: ['./src/assets'],
+      optimization: true,
+      outputHashing: 'none',
+      generatePackageJson: false,
+      sourceMap: false,
+      bundle: true,
+      externals: [] // 🔑 inclut toutes les libs dans le main.js
+    }),
   ],
 };
